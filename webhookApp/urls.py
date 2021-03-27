dbbdb7313aa38c064e4b77d90aa4a60ed4d13bcc
smoothbronx <vlad.uhwatov@yandex.ru>
2021-03-27 17:33:36 +0200

    Changed urls.

--- webhookApp/urls.py
+++ webhookApp/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('webhook/discord', include('discord.urls'))
+    path('webhook/discord/', include('discord.urls'))
 ]
